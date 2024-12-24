4579f11808691f313bd3277e41c2cf728744242e
dnx04 <86938955+dnx04@users.noreply.github.com>
2024-12-24 14:10:31 +0700

    fix

--- tools/scripts/bootstrap.sh
+++
@@ -1,4 +0,0 @@
-uv venv
-source .venv/bin/activate
-uv pip install git+https://github.com/Pennyw0rth/NetExec
-git submodule add https://github.com/danielmiessler/SecLists.git tools/SecLists --depth 1
